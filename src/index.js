e8523478b2bd60b6f00589a2e19384b53b2df43f
valex <val.matej99@gmail.com>
2024-12-26 08:49:01 +0100

    Creating nav bar

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import "./styles.css";
+
+import projectIcon from "./images/check-icon.png";
+
+console.log("aaa   OK");
+
+const nav = document.querySelector("nav");
+
+const navDiv = document.createElement("div");
+navDiv.classList.add("nav-div");
+
+const icon = document.createElement("img");
+icon.classList.add("icon");
+icon.src = projectIcon;
+icon.alt = "ToDo Icon";
+
+const title = document.createElement("h1");
+title.classList.add("project-title");
+title.textContent = "TODO List";
+
+const githubButton = document.createElement("button");
+githubButton.classList.add("github-button");
+githubButton.textContent = "View on Github";
+
+navDiv.appendChild(icon);
+navDiv.appendChild(title);
+navDiv.appendChild(githubButton);
+
+nav.appendChild(navDiv);
